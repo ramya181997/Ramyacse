e94b6de0838513f4967a954edb7b4d8dfa282929
ramya181997 <32761005+ramya181997@users.noreply.github.com>
2017-12-03 22:46:36 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Number
+{
+public static void main(String arg[])
+{
+Scanner ss=new Scanner(System.in);
+int a=ss.nextInt();
+if(a℅2==0)
+{
+System.out.println(even);
+}
+else
+{
+System.out.println(odd);
+}
+}
+}
+
