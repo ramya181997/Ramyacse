0ca900449b697a7c3f54e4a589b3c1d9710a0435
ramya181997 <32761005+ramya181997@users.noreply.github.com>
2018-02-28 11:01:34 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+class large
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in)
+int a,b,c;
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+if((a>b)||(b>a))
+{
+System.out.println(a is:+Greater);
+}
+else
+{
+System.out.println(b is:+greater);
+}
+else if((b>c)||(c>b))
+{
+System.out.println(c is:+ggreater);
+}
+else
+{
+System.out.println(b is:Greater);
+}
+else if((a>c)||(c>a))
+{
+System.out.println(a is:+greater);
+}
+else
+{
+System.out.println(c is:+greater);
+}
+}
+}
