7b9abfaedf95b17c0eecce5bc0fddf81a646032e
ramya181997 <32761005+ramya181997@users.noreply.github.com>
2018-02-28 11:16:00 +0530

    Update large.java

--- large.java
+++ large.java
@@ -9,27 +9,15 @@ int a,b,c;
 a=sc.nextInt();
 b=sc.nextInt();
 c=sc.nextInt();
-if((a>b)||(b>a))
+if(a>b&&a>c)
 {
 System.out.println(a is:+Greater);
 }
-else
-{
-System.out.println(b is:+greater);
-}
-else if((b>c)||(c>b))
-{
-System.out.println(c is:+greater);
-}
-else
+else if(b>a&&b>c)
 {
 System.out.println(b is:Greater);
 }
-else if((a>c)||(c>a))
-{
-System.out.println(a is:+greater);
-}
-else
+else if(c>b&&c>a)
 {
 System.out.println(c is:+greater);
 }
