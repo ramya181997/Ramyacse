d3f36c562f85cd029bc8528e26f04d3a6bf3a0c3
ramya181997 <32761005+ramya181997@users.noreply.github.com>
2018-06-09 22:05:04 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i=sc.nextInt();
+while(1<i)
+{
+System.out.println("Hello");
+i++;
+}
+}
+}
