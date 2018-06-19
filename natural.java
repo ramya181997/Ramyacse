fbe631a34a5b848f44040bc2c8c73bdda10c0564
ramya181997 <32761005+ramya181997@users.noreply.github.com>
2018-06-19 12:14:55 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class natural
+{
+    public static void main(String args[])
+    {
+        int sum=0,i,n=5;
+        for(i=1;i<n;i++)
+        {
+        for(i=0;i<n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.print(sum);
+    }
+}
+}
