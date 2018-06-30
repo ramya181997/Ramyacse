62071b9588fa51eb6c42584dc21d66707133ea62
ramya181997 <32761005+ramya181997@users.noreply.github.com>
2018-06-30 15:37:18 +0530

    Update and rename ramu.java to sherlin.java

---
+++ sherlin.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class sherlin
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=5;i++)
+        {
+          int k;
+        k=a*i;
+
+            System.out.println(k+" ");
+        }
+    }
+}
+
